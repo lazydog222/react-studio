f8c8ded36992b91078205d9df38be9a80c2bbafe
Henry Schaefer <henryschaefer@henrys-air.myfiosgateway.com>
2022-11-09 16:38:36 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from './components/BakeryItem.js';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -12,17 +13,25 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [cart, updateCart] = useState(0);
+
+  const [items, updateItems] = useState([]);
+
+
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Hank's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem name = {item.name} description = {item.description} price = {item.price} cart = {cart} updateCart = {updateCart} items = {items} updateItems = {updateItems}
+        image ={item.image}/> // replace with BakeryItem component
       ))}
-
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div>
+        {items.map(a => (<p>${a[0]} for {a[1]}</p>))}
+        </div>
+        <h> Total: ${cart} </h>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+
+
+export default function BakeryItem(props) {
+    const handleClick = () => {
+        {props.updateCart((prevCount) => prevCount + props.price)}
+        {props.items.push([props.price, props.name])}
+    }
+
+
+    return (
+      <div class = "BakeryItem">
+        <img src = {props.image}/>
+        <h>{props.name}</h>
+        <p>{props.description}</p>
+        <p>{props.price}</p>
+        <button onClick = {handleClick} class = "button"> Add to Cart </button>
+      </div>  
+    );
+}
